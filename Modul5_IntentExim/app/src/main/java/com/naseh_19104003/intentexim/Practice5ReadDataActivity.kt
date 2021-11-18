02ef1e2267f9a7187c130a502b03738c444cd9fd
nasehhibban <72422096+nasehhibban@users.noreply.github.com>
2021-11-18 17:10:08 +0700

    Add files via upload

---
+++ Modul5_IntentExim/app/src/main/java/com/naseh_19104003/intentexim/Practice5ReadDataActivity.kt
@@ -0,0 +1,25 @@
+package com.naseh_19104003.intentexim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naseh_19104003.intentexim.databinding.ActivityPractice5ReadDataBinding
+
+class Practice5ReadDataActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPractice5ReadDataBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPractice5ReadDataBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val prodi = intent.getStringExtra(EXTRA_PRODI)
+        binding.tvProdiSaya.text = "Prodi Saya Adalah $prodi"
+    }
+
+
+
+    companion object {
+        const val EXTRA_PRODI = "extra_prodi"
+    }
+}
