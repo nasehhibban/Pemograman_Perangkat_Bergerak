57a2df92fed51f9a7964439f3d9b51b106690f04
nasehhibban <72422096+nasehhibban@users.noreply.github.com>
2022-01-24 20:12:28 +0700

    Update ExampleInstrumentedTest.kt

--- Modul10/app/src/androidTest/java/com/naseh_19104003/modul10/ExampleInstrumentedTest.kt
+++ Modul10/app/src/androidTest/java/com/naseh_19104003/modul10/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rifqi_19104031.modul10", appContext.packageName)
+        assertEquals("com.naseh_19104003.modul10", appContext.packageName)
     }
 }
